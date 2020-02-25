f92fdc13471487f10ae87505b6fac8d99d40eb83
prasanth132910 <prasanthpkp191196@gmail.com>
2020-02-25 10:22:28 +0530

    Higher Order Function

---
+++ app.js
@@ -0,0 +1,57 @@
+var foods = ["pizza", "burger", "fingerchips", "donuts", "springroll"];
+let modifiedfoods = foods.slice(1, 4);
+console.log(modifiedfoods);
+
+foods.splice(2, 0, "icecream", "fingerchips");
+console.log(foods);
+
+
+const numberArray = [12, 324, 213, 4, 2, 3, 45, 4234];
+let evennum = numberArray.filter(ele => ele%2==0) 
+console.log(evennum);
+
+
+//filter
+function isPrime(numberArray) {
+    prime = numberArray.filter((number) => {
+        for (var i = 2; i <= Math.sqrt(number); i++)
+         {
+            if (number % i === 0) 
+            return false;
+        }
+        return true;
+    });
+
+    console.log(prime);
+}
+
+
+isPrime(numberArray);
+
+
+
+
+//  Map
+const myArray = [11, 34, 20, 5, 53, 16];
+function findSquareOfNumbers(myArray)
+{
+    var map = myArray.map(element => element * element);
+    return map;
+}
+console.log(findSquareOfNumbers(myArray));
+
+
+
+
+
+
+
+// Reduce
+function multiply(map)
+{
+let sum = map.reduce(function (i, element) {
+    return i + element;
+});
+    console.log(sum);
+}
+multiply(findSquareOfNumbers(myArray));
